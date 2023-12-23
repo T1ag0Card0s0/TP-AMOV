641d57bd97f8fbb85e5a0577e022783546ae4ebd
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2023-12-23 21:16:30 +0000

    Updates place of interest image

--- TP1/app/src/main/java/pt/isec/amov/tp1/ui/composables/ListLocals.kt
+++ TP1/app/src/main/java/pt/isec/amov/tp1/ui/composables/ListLocals.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,9 +17,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
@@ -70,7 +74,7 @@ fun ListLocals(
                             fontSize = 20.sp
                         )
                         Spacer(modifier = Modifier.height(16.dp))
-                        if (it.imageUri != null) {
+                        if(it.imageUri!=null){
                             AsyncImage(
                                 model =it.imageUri!!,
                                 contentDescription = "Local image",
